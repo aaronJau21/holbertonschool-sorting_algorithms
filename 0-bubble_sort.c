28e9fbd8d2a049f7c563484e716f1111a65af629
JuliethAnarosa <america02anarosa@gmail.com>
2022-08-25 00:48:36 -0500

    fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,8 +6,8 @@ void bubble_sort(int *array, size_t size)
 
     if (size < 2)
         return;
-    for (i = 0; i < size; i++)
-        for (index = 0; index < size; index++)
+    for (i = 0; i < size - 1; i++)
+        for (index = 0; index < size - 1; index++)
         {
             if (array[index] > array[index + 1] && array[index + 1])
             {
