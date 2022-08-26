24d53148edd0a5ad0f5da9d61708ac29e7624d2d
Aaron <aaronjau21@gmail.com>
2022-08-26 12:09:59 -0500

    sort.h

--- sort.h
+++ sort.h
@@ -24,5 +24,6 @@ void print_array(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap(listint_t **list, listint_t *current, listint_t *previous);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
